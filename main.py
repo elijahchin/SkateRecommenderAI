5a6e587b6ccf358c026b63ef325bcd2ae7150a47
Elijah <elijahchin1@gmail.com>
2025-05-14 04:48:00 -0400

    slowly but surely

--- main.py
+++ main.py
@@ -3,5 +3,16 @@ import json
 with open ('tricks.json','r') as file:
     data = json.load(file)
 
-print(data)
-user_tricks = ["ollie","frontside 180","pop shuvit"]
+
+user_tricks = ["ollie","frontside 180","pop shuvit","kickflip"]
+
+known_tricks = [trick for trick in data if ['name'] in user_tricks]
+uknown_tricks = [trick for trick in data if ['name'] not in user_tricks]
+
+unique_tags = set()
+
+for trick in data:
+    for tags in data['tags']:
+        unique_tags.add(tags)
+
+unique_tags = sorted(list(unique_tags))     
